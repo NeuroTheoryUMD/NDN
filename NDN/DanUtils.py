90f82b586ab340d575e015eee1f813558650a49c
Dan Butts <dbutts@Peabrain.local>
2018-06-17 22:03:03 -0400

    update to reg_path (DanUtils)

--- NDN/DanUtils.py
+++ NDN/DanUtils.py
@@ -2,7 +2,7 @@
 
 from __future__ import division
 import numpy as np
-import NDN.NDN as NDN
+import NDN as NDN
 #import NDN.NDNutils as NDNutils
 
 
@@ -11,8 +11,8 @@ def reg_path(
         NDNmodel=None,
         input_data=None,
         output_data=None,
-        train_inds=None,
-        test_inds=None,
+        train_indxs=None,
+        test_indxs=None,
         reg_type='l1',
         reg_vals=[1e-6, 1e-4, 1e-3, 1e-2, 0.1, 1],
         ffnet_n=0,
@@ -36,11 +36,11 @@ def reg_path(
     if input_data is None:
         raise TypeError('Must specify input_data.')
     if output_data is None:
-        raise TypeError('Must specify input_data.')
-    if train_inds is None:
-        raise TypeError('Must specify input_data.')
-    if test_inds is None:
-        raise TypeError('Must specify input_data.')
+        raise TypeError('Must specify output_data.')
+    if train_indxs is None:
+        raise TypeError('Must specify training indices.')
+    if test_indxs is None:
+        raise TypeError('Must specify testing indices.')
 
     num_regs = len(reg_vals)
 
@@ -51,10 +51,10 @@ def reg_path(
         test_mod = NDNmodel.copy_model()
         test_mod.set_regularization( reg_type, reg_vals[nn], ffnet_n, layer_n )
         test_mod.train(input_data=input_data, output_data=output_data,
-                       train_indxs=train_inds, test_inds=test_inds,
+                       train_indxs=train_indxs, test_indxs=test_indxs,
                        data_filters=data_filters, learning_alg='adam', opt_params=opt_params)
         LLxs[nn]=np.mean(
-            test_mod.eval_models(input_data=input_data,output_data=output_data,data_indxs=test_inds))
+            test_mod.eval_models(input_data=input_data,output_data=output_data,data_indxs=test_indxs))
         test_mods.append( test_mod.copy_model() )
         print( nn, '(', reg_type, '=', reg_vals[nn], '): ', LLxs[nn])
 
